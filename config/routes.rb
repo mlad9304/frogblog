30be8f399d18c250a2934af7706b17874fb165bf
rndev80 <jianmyphy123@gmail.com>
2018-08-13 09:45:19 -0700

    navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'about' => 'posts#about', as: :about
+  get 'contact' => 'posts#contact', as: :contact
+
   root to: 'posts#index'
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
