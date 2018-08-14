8c3e64e7d6d4416198e861ff57d4b83ceb8885ea
rndev80 <jianmyphy123@gmail.com>
2018-08-14 08:53:50 -0700

    navbar active menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'about' => 'posts#about', as: :about
-  get 'contact' => 'posts#contact', as: :contact
+  get 'about' => 'pages#about', as: :about
+  get 'contact' => 'pages#contact', as: :contact
 
   root to: 'posts#index'
   resources :posts
